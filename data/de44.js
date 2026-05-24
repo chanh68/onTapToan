d3f931835d6aa4e50c113dc9f07bf3e84656c2da
chanh68 <142521646+chanh68@users.noreply.github.com>
2026-05-24 17:03:13 +0700

    dapan434445

---
+++ data/de44.js
@@ -0,0 +1,31 @@
+examAnswers["de44"] = {
+    "examName": "Đề Số 44 - Đề môn Toán 2026",
+    "part1": [
+        { "id": 1, "ans": "B", "exp": "", "image": "", "video": "" },
+        { "id": 2, "ans": "B", "exp": "", "image": "", "video": "" },
+        { "id": 3, "ans": "A", "exp": "", "image": "", "video": "" },
+        { "id": 4, "ans": "B", "exp": "", "image": "", "video": "" },
+        { "id": 5, "ans": "C", "exp": "", "image": "", "video": "" },
+        { "id": 6, "ans": "D", "exp": "", "image": "", "video": "" },
+        { "id": 7, "ans": "B", "exp": "", "image": "", "video": "" },
+        { "id": 8, "ans": "A", "exp": "", "image": "", "video": "" },
+        { "id": 9, "ans": "D", "exp": "", "image": "", "video": "" },
+        { "id": 10, "ans": "C", "exp": "", "image": "", "video": "" },
+        { "id": 11, "ans": "C", "exp": "", "image": "", "video": "" },
+        { "id": 12, "ans": "B", "exp": "", "image": "", "video": "" }
+    ],
+    "part2": [
+        { "id": 1, "ans": ["Đ", "S", "S", "S"], "exp": "", "image": "", "video": "" },
+        { "id": 2, "ans": ["Đ", "S", "S", "Đ"], "exp": "", "image": "", "video": "" },
+        { "id": 3, "ans": ["S", "Đ", "Đ", "Đ"], "exp": "", "image": "", "video": "" },
+        { "id": 4, "ans": ["S", "Đ", "Đ", "Đ"], "exp": "", "image": "", "video": "" }
+    ],
+    "part3": [
+        { "id": 1, "ans": "0.81", "exp": "", "image": "", "video": "" },
+        { "id": 2, "ans": "0.79", "exp": "", "image": "", "video": "" },
+        { "id": 3, "ans": "6", "exp": "", "image": "", "video": "" },
+        { "id": 4, "ans": "787", "exp": "", "image": "", "video": "" },
+        { "id": 5, "ans": "14", "exp": "", "image": "", "video": "" },
+        { "id": 6, "ans": "652", "exp": "", "image": "", "video": "" }
+    ]
+};
---
+++ data/de45.js
@@ -0,0 +1,31 @@
+examAnswers["de45"] = {
+    "examName": "Đề Số 45 - Đề môn Toán 2026",
+    "part1": [
+        { "id": 1, "ans": "A", "exp": "", "image": "", "video": "" },
+        { "id": 2, "ans": "A", "exp": "", "image": "", "video": "" },
+        { "id": 3, "ans": "D", "exp": "", "image": "", "video": "" },
+        { "id": 4, "ans": "C", "exp": "", "image": "", "video": "" },
+        { "id": 5, "ans": "C", "exp": "", "image": "", "video": "" },
+        { "id": 6, "ans": "B", "exp": "", "image": "", "video": "" },
+        { "id": 7, "ans": "D", "exp": "", "image": "", "video": "" },
+        { "id": 8, "ans": "C", "exp": "", "image": "", "video": "" },
+        { "id": 9, "ans": "A", "exp": "", "image": "", "video": "" },
+        { "id": 10, "ans": "C", "exp": "", "image": "", "video": "" },
+        { "id": 11, "ans": "C", "exp": "", "image": "", "video": "" },
+        { "id": 12, "ans": "A", "exp": "", "image": "", "video": "" }
+    ],
+    "part2": [
+        { "id": 1, "ans": ["Đ", "S", "Đ", "S"], "exp": "", "image": "", "video": "" },
+        { "id": 2, "ans": ["Đ", "S", "S", "Đ"], "exp": "", "image": "", "video": "" },
+        { "id": 3, "ans": ["Đ", "Đ", "S", "S"], "exp": "", "image": "", "video": "" },
+        { "id": 4, "ans": ["Đ", "S", "S", "Đ"], "exp": "", "image": "", "video": "" }
+    ],
+    "part3": [
+        { "id": 1, "ans": "4", "exp": "", "image": "", "video": "" },
+        { "id": 2, "ans": "135", "exp": "", "image": "", "video": "" },
+        { "id": 3, "ans": "1180", "exp": "", "image": "", "video": "" },
+        { "id": 4, "ans": "1.28", "exp": "", "image": "", "video": "" },
+        { "id": 5, "ans": "473", "exp": "", "image": "", "video": "" },
+        { "id": 6, "ans": "17.7", "exp": "", "image": "", "video": "" }
+    ]
+};
